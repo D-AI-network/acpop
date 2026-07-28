d77301ebed63e8800e96f8f1f1efc44e3c06f821
D <deonisialm@gmail.com>
2026-07-28 13:22:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ html, body, [class*="css"] {
 }
 
 .block-container {
-  max-width: 420px !important;
+  max-width: 430px !important;
   padding: 0 !important;
   margin: 0 auto !important;
   min-height: 100vh;
